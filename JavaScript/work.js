eb144fcd30b2fad845125eb1b8eb077c93c52904
AlexHdz16 <alexander.hernandeztamayo@edu.sait.ca>
2024-11-16 13:06:11 -0700

    added button elements
    added alert button elements for text display, after click text, and hover

--- JavaScript/work.js
+++ JavaScript/work.js
@@ -6,3 +6,19 @@ yearElement.textContent = currentYear;
 
 // Date on footer//
 console.log("Today's Date:", currentDate.toDateString());
+
+// Alert Button //
+const alertButton = document.getElementById("btn-alert");
+
+// Alert on click //
+alertButton.addEventListener("click", () => {
+    alert("Why won't you follow simple instructions?");
+});
+
+// Change of text //
+alertButton.onmouseover = function () {
+    alertButton.innerText = "I said DON'T!";
+};
+alertButton.onmouseout = function () {
+    alertButton.innerText = "Don't Click Me";
+};
