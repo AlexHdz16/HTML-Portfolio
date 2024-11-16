61de7f07ddc06a84fe4f1cb9a312be013a5ebc30
AlexHdz16 <alexander.hernandeztamayo@edu.sait.ca>
2024-11-16 12:29:39 -0700

    added js footer
    aded all elements so teh footer displays correctly

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -18,3 +18,12 @@ if (currentHour < 12) {
 
 greetingDiv.innerText = greetingMessage;
 greetingDiv.classList.add(greetingClass);
+
+// Footer //
+const yearElement = document.getElementById("current-year");
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+yearElement.textContent = currentYear;
+
+// Date on footer//
+console.log("Today's Date:", currentDate.toDateString());
