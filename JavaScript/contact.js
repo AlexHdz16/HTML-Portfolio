d05c17cf738d0d642492c1fb1a554e206c0aa5ec
AlexHdz16 <alexander.hernandeztamayo@edu.sait.ca>
2024-11-16 17:15:26 -0700

    added the loop on numbers list

--- JavaScript/contact.js
+++ JavaScript/contact.js
@@ -6,3 +6,13 @@ yearElement.textContent = currentYear;
 
 // Date on footer//
 console.log("Today's Date:", currentDate.toDateString());
+
+// List id element//
+const numberList = document.getElementById('numbers');
+
+// For Loop//
+for (let i = 1; i <= 12; i++) {
+    const listItem = document.createElement('li');
+    listItem.textContent = (i % 2 === 0) ? `Even - ${i}` : `Odd - ${i}`;
+    numberList.appendChild(listItem);
+}
